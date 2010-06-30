8fb2b57276f522e8a5c7ad2ddbd7c8d3830bf98a
Pedro Carriço <pecarrico@pecarrico.local>
2010-07-01 06:41:50 +0800

    Implemented the ability to tell the bot to shut up and to wake up.

--- src/drinkalot/bot/drunk/DrunkBot.java
+++ src/drinkalot/bot/drunk/DrunkBot.java
@@ -11,8 +11,14 @@ import java.util.Random;
 
 public class DrunkBot extends PircBot {
 
+    boolean mute = false;
+    Random generator;
+    int insultModifier;
+
     public DrunkBot() {
         this.setName("DrunkBot");
+        generator = new Random();
+        insultModifier = 3;
     }
 
     public Calendar calculateNextFridayAt18PM() {
@@ -34,8 +40,8 @@ public class DrunkBot extends PircBot {
         String insult = "You heathen swine";
 
         try {
-            URL yahoo = new URL("http://www.webinsult.com/");
-            BufferedReader bf = new BufferedReader(new InputStreamReader(yahoo.openStream()));
+            URL webinsult = new URL("http://www.webinsult.com/");
+            BufferedReader bf = new BufferedReader(new InputStreamReader(webinsult.openStream()));
             String inputLine;
 
             while ((inputLine = bf.readLine()) != null) {
@@ -53,26 +59,58 @@ public class DrunkBot extends PircBot {
         return insult;
     }
 
+    private boolean insultTime() {
+            return generator.nextInt(10) < insultModifier;
+    }
+
     public void onMessage(String channel, String sender, String login, String hostname, String message) {
 
         String time = new java.util.Date().toString();
         Calendar now = Calendar.getInstance();
         Calendar nextFridayAt18PM = calculateNextFridayAt18PM();
-        Random generator = new Random();
 
-       if (generator.nextInt(10)<5 ){
-            sendMessage(channel, sender + ", " + getInsult());
-            return;
-       }
 
-        if (message.equalsIgnoreCase("time")) {
-            sendMessage(channel, sender + ": The time is now " + time);
+        if (mute) {
+            if (message.toLowerCase().contains("wake up")) {
+                sendMessage(channel, sender + ", I am awake now");
+                mute = false;
+            }
+        } else {
+            if (message.toLowerCase().contains("shut up")) {
+                sendMessage(channel, sender + ", I'll be quiet from now on...bastard!");
+                mute = true;
+                return;
+            }
 
-        } else if (message.toLowerCase().contains("jolas")) {
+            if (message.toLowerCase().contains("more insults")) {
+                sendMessage(channel, sender + ", You're still not satisfied?!");
+                if (insultModifier < 10)
+                    insultModifier+=1;
+                return;
+            }
 
-            long timeLeftToBeer = (nextFridayAt18PM.getTimeInMillis() - now.getTimeInMillis()) / (1000 * 60);
-            sendMessage(channel, "Still " + timeLeftToBeer + " minutes left to drink beer :-(");
+            if (message.toLowerCase().contains("less insults")) {
+                sendMessage(channel, sender + ", I'll keep a lower profile from now on...");
+                if (insultModifier > 1)
+                    insultModifier-=1;
+                return;
+            }
+
+            if (insultTime()){
+                 sendMessage(channel, sender + ", " + getInsult());
+                 return;
+            }
+
+            if (message.equalsIgnoreCase("time")) {
+                sendMessage(channel, sender + ": The time is now " + time);
+                return;
+            }
+
+            if (message.toLowerCase().contains("jolas")) {
+                long timeLeftToBeer = (nextFridayAt18PM.getTimeInMillis() - now.getTimeInMillis()) / (1000 * 60);
+                sendMessage(channel, "Still " + timeLeftToBeer + " minutes left to drink beer :-(");
+                return;
+            }
         }
-
     }
 }
